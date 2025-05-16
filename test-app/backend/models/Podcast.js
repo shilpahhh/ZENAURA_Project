9563423e109746905c9c82e274c0de9a7b9e411b
shilpahhh <silpababu2005@gmail.com>
2025-05-17 01:46:16 +0800

    Create Podcast.js

---
+++ test-app/backend/models/Podcast.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const podcastSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  file: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Podcast", podcastSchema); 
